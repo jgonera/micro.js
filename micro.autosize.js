a329b1400c5f90075aad3d464be1e25a9dd301b0
Juliusz Gonera <jgonera@wikimedia.org>
2013-11-08 13:10:30 -0800

    Fix max.autosize for big textareas
    Previously, the page would scroll back to the top in certain situations
    if the textarea was taller than screen height.

--- micro.autosize.js
+++ micro.autosize.js
@@ -4,15 +4,21 @@
  */
 
 ;(function($) {
+  var $window = $(window);
+
   $.fn.microAutosize = function() {
     var $el = this;
 
     $el.on('input', function() {
+      var scrollTop;
+
       if ($el.prop('scrollHeight')) {
+        scrollTop = $window.scrollTop();
         $el.
           css('height', 'auto').
           // can't reuse prop('scrollHeight') because we need the current value
-          css('height', ($el.prop( 'scrollHeight' ) + 2) + 'px');
+          css('height', ($el.prop('scrollHeight') + 2) + 'px');
+        $window.scrollTop(scrollTop);
       }
     });
 
